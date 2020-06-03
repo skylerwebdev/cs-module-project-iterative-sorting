bc78dc1e3a56920275ad4c4d6fbc68e62be0d74c
Skyler Dowdy <skylerwebdev@gmail.com>
2020-06-03 18:36:19 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,23 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
+    #check for empty array
+    if len(arr) == 0:
+        return -1
+    l = 0
+    h = len(arr)-1
+    while h > l:
+        m = (l+h) // 2
+        if arr[m] == target:
+            return m
+        elif arr[m] > target:
+            h = m
+        elif arr[m] < target:
+            l = m + 1
 
     return -1  # not found
